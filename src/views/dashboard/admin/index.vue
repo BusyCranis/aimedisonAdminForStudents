efe51647e3e4da4b0d2d8f0183320206f614d5c0
cranis <cranis2@naver.com>
2025-05-29 12:05:51 +0900

    efwfeefwefwfewfefewefwewfewfewweffeweef

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -93,7 +93,10 @@ export default {
     return {
       chart: null,
 
-      dailyStatResponse: {}
+      dailyStatResponse: {},
+
+      yearValue: [2024, 2025],
+      monthValue: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
     }
   },
