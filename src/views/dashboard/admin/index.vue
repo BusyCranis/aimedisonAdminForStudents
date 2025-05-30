afb56a86ec81664f050517257bf2455074711c4e
cranis <cranis2@naver.com>
2025-05-30 18:34:14 +0900

    efwewfefwwwefwefefewweffewwefweffewwwfeweffwe

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -740,6 +740,92 @@ export default {
 
 
     async setChatTokenCountAllPeriod() {
+      console.log(this.dailyStatResponse.data)
+      const monthlyKeyNamesForAggr = Object.keys(this.dailyStatResponse.data)
+      const dailyValueArray = []
+
+      console.log(monthlyKeyNamesForAggr)
+
+      for (let i = 0; i < monthlyKeyNamesForAggr.length; i++) {
+        let dailyChatTokenCount = 0;
+
+
+
+        console.log(this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]])
+        for(let j = 0; j <= this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]].length; j++) {
+          // console.log(this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]][j].answerObjectDetail.tokenlength.total_tokens);
+          if(j <= this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]].length - 1) {
+            console.log(this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]][j].answerObjectDetail.tokenlength.total_tokens);
+            dailyChatTokenCount = dailyChatTokenCount + this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]][j].answerObjectDetail.tokenlength.total_tokens;
+          } else if(j === this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]].length) {
+            dailyValueArray.push(dailyChatTokenCount);
+          }
+        }
+
+      }
+
+
+
+
+
+
+
+      console.log(dailyValueArray)
+
+      this.tokenCountChart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          top: 10,
+          left: '2%',
+          right: '2%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [{
+          type: 'category',
+          // data: ['월요일', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+          data: monthlyKeyNamesForAggr,
+          axisTick: {
+            alignWithLabel: true
+          }
+        }],
+        yAxis: [{
+          type: 'value',
+          axisTick: {
+            show: false
+          }
+        }],
+        series: [{
+          name: '사용량(글자 수)',
+          type: 'bar',
+          stack: 'vistors',
+          barWidth: '60%',
+          // data: [79, 52, 200, 134, 190, 130],
+          data: dailyValueArray,
+          animationDuration
+        }
+        // {
+        //   name: 'pageB',
+        //   type: 'bar',
+        //   stack: 'vistors',
+        //   barWidth: '60%',
+        //   data: [80, 52, 200, 334, 390, 330, 220],
+        //   animationDuration
+        // }, {
+        //   name: 'pageC',
+        //   type: 'bar',
+        //   stack: 'vistors',
+        //   barWidth: '60%',
+        //   data: [30, 52, 200, 334, 390, 330, 220],
+        //   animationDuration
+        // }
+        ]
+      })
 
 
 
