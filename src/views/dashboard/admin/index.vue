2e89b63003ac0d1e06e747f5ab40c707c7d331cb
cranis <cranis2@naver.com>
2025-05-30 18:07:08 +0900

    ewfewfewfewfwefefwewfewfewfefefwwefweffeweefwwefwef

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -649,8 +649,6 @@ export default {
         for (let i = 0; i < monthlyKeyNamesForAggr.length; i++) {
           let dailyChatTokenCount = 0;
 
-
-
           console.log(searchResult.data[monthlyKeyNamesForAggr[i]])
           for(let j = 0; j <= searchResult.data[monthlyKeyNamesForAggr[i]].length; j++) {
          
@@ -666,6 +664,63 @@ export default {
 
 
 
+
+
+
+        this.tokenCountChart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+              type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          grid: {
+            top: 10,
+            left: '2%',
+            right: '2%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [{
+            type: 'category',
+            // data: ['월요일', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            data: monthlyKeyNamesForAggr,
+            axisTick: {
+              alignWithLabel: true
+            }
+          }],
+          yAxis: [{
+            type: 'value',
+            axisTick: {
+              show: false
+            }
+          }],
+          series: [{
+            name: '대화 수',
+            type: 'bar',
+            stack: 'vistors',
+            barWidth: '60%',
+            // data: [79, 52, 200, 134, 190, 130],
+            data: dailyValueArray,
+            animationDuration
+          }
+          // {
+          //   name: 'pageB',
+          //   type: 'bar',
+          //   stack: 'vistors',
+          //   barWidth: '60%',
+          //   data: [80, 52, 200, 334, 390, 330, 220],
+          //   animationDuration
+          // }, {
+          //   name: 'pageC',
+          //   type: 'bar',
+          //   stack: 'vistors',
+          //   barWidth: '60%',
+          //   data: [30, 52, 200, 334, 390, 330, 220],
+          //   animationDuration
+          // }
+          ]
+        })
 
 
 
