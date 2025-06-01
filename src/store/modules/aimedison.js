758b8e99842a5af52827f12472e32b59127bec3f
cranis <cranis2@naver.com>
2025-06-01 15:21:57 +0900

    weewfewweffwefwefewweewf

--- src/store/modules/aimedison.js
+++ src/store/modules/aimedison.js
@@ -5,7 +5,7 @@ const state = {
 //     opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
 //     withoutAnimation: false
 //   },
-  device: 'desktop',
+  aimedisonKeywordUserList: [],
 //   size: Cookies.get('size') || 'medium'
 }
 
