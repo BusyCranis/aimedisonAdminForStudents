b0efcdf807d2d200d859e77fc83dc83bdc53665a
cranis <cranis2@naver.com>
2025-06-02 12:40:07 +0900

    ewfefwefwwefefwewfwefewfwefweweffwe

--- src/store/modules/aimedison.js
+++ src/store/modules/aimedison.js
@@ -1,33 +1,16 @@
 // import Cookies from 'js-cookie'
 
 const state = {
-//   sidebar: {
-//     opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
-//     withoutAnimation: false
-//   },
+
   aimedisonKeywordUserList: [],
-  
+
   recentScsdChatHistoryOfSlctdUser: [],
 
 
-//   size: Cookies.get('size') || 'medium'
 }
 
 const mutations = {
-//   TOGGLE_SIDEBAR: state => {
-//     state.sidebar.opened = !state.sidebar.opened
-//     state.sidebar.withoutAnimation = false
-//     if (state.sidebar.opened) {
-//       Cookies.set('sidebarStatus', 1)
-//     } else {
-//       Cookies.set('sidebarStatus', 0)
-//     }
-//   },
-//   CLOSE_SIDEBAR: (state, withoutAnimation) => {
-//     Cookies.set('sidebarStatus', 0)
-//     state.sidebar.opened = false
-//     state.sidebar.withoutAnimation = withoutAnimation
-//   },
+
   setAimedisonKeywordUserList: (state, payload) => {
     state.aimedisonKeywordUserList = payload
   },
@@ -35,10 +18,7 @@ const mutations = {
   setRecentScsdChatHistoryOfSlctdUser: (state, payload) => {
     state.recentScsdChatHistoryOfSlctdUser = payload
   }
-//   SET_SIZE: (state, size) => {
-//     state.size = size
-//     Cookies.set('size', size)
-//   }
+
 }
 
 const actions = {
