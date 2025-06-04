756117786119987e55163c50162169e6a43c7173
cranis <cranis2@naver.com>
2025-06-04 11:54:13 +0900

    ewfefwefwwefefwefefwwfeefefwewf

--- src/views/example/components/ArticleDetail.vue
+++ src/views/example/components/ArticleDetail.vue
@@ -120,17 +120,7 @@ const defaultForm = {
 
 
 
-  // status: 'draft',
-  // title: '', // 文章题目
-  // content: '', // 文章内容
-  // content_short: '', // 文章摘要
-  // source_uri: '', // 文章外链
-  // image_uri: '', // 文章图片
-  // display_time: undefined, // 前台展示时间
-  // id: undefined,
-  // platforms: ['a-platform'],
-  // comment_disabled: false,
-  // importance: 0
+ 
 }
 
 export default {
