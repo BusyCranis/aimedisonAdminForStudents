575f17b4b5ff540f7c1ea1191d02d72a38be94ce
cranis <cranis2@naver.com>
2025-06-04 15:10:21 +0900

    ewfewfefwewfwefefwefwfewwfefewfeweffew

--- src/views/example/components/ArticleDetail.vue
+++ src/views/example/components/ArticleDetail.vue
@@ -341,9 +341,7 @@ export default {
       let headerRedFactor = parseInt(hexadecimalHeaderCode.slice(1, 3), 16);
       let headerGreenFactor = parseInt(hexadecimalHeaderCode.slice(3, 5), 16);
       let headerBlueFactor = parseInt(hexadecimalHeaderCode.slice(5, 7), 16);
-
-  
-      chatbotHeaderColor = `rgb(${headerRedFactor}, ${headerGreenFactor}, ${headerBlueFactor})`;
+      // chatbotHeaderColor = `rgb(${headerRedFactor}, ${headerGreenFactor}, ${headerBlueFactor})`;
 
 
 
@@ -354,7 +352,7 @@ export default {
       const keywordAppUiConfigOption = {
         shopAppTitle: this.postForm.uiConfigOption.shopAppTitle,
         shopAppColors: {
-          headerColor: this.postForm.uiConfigOption.shopAppColors.headerColor,
+          headerColor: `rgb(${headerRedFactor}, ${headerGreenFactor}, ${headerBlueFactor})`,
           mainSpaceColor: this.postForm.uiConfigOption.shopAppColors.mainSpaceColor,
           inputBoxColor: this.postForm.uiConfigOption.shopAppColors.inputBoxColor,
           userMessageBoxColor: this.postForm.uiConfigOption.shopAppColors.userMessageBoxColor,
