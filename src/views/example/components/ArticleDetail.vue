ffdbd00f51ebe74fffe83065b7cef3b1af862da8
cranis <cranis2@naver.com>
2025-06-05 12:34:49 +0900

    efwfwefefwefwfwefwefefwewffew

--- src/views/example/components/ArticleDetail.vue
+++ src/views/example/components/ArticleDetail.vue
@@ -73,7 +73,7 @@
 
 
         <el-row :gutter="8">
-          <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 8}" :lg="{span: 6}" :xl="{span: 4}" style="padding-right:8px;margin-bottom:0px;">
+          <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 8}" :lg="{span: 6}" :xl="{span: 4}" style="padding-left:50px;  padding-right:0px;margin-bottom:0px;">
             <TransactionTable />
           </el-col>
         </el-row>
