9d5d105c9194433c3bb310c589d3f4a8e568a20b
cranis <cranis2@naver.com>
2026-01-08 15:51:53 +0900

    fweaewfaewfaefwewafaewfaewewfaewewaffaffefaafwewafe

--- src/views/dashboard/admin/components/TransactionTable.vue
+++ src/views/dashboard/admin/components/TransactionTable.vue
@@ -1,139 +1,120 @@
 <template>
-  <el-table :data="$store.state.aimedison.aimedisonKeywordUserList" style="width: 100%; padding-top: 0px;">
+  <el-table
+    :data="$store.state.aimedison.aimedisonKeywordUserList"
+    style="width: 100%; padding-top: 0px"
+  >
     <el-table-column label="사용자 아이디" min-width="100">
       <template slot-scope="scope">
         {{ scope.row.memberId | orderNoFilter }}
       </template>
     </el-table-column>
-    <!-- <el-table-column label="Price" width="195" align="center">
-      <template slot-scope="scope">
-        ¥{{ scope.row.price | toThousandFilter }}
-      </template>
-    </el-table-column> -->
+
     <el-table-column label="" width="150" align="center">
-
-
-      <template slot-scope="{row}">
-
-        <el-button    type="button"    @click="findAllChatHistory(row.memberId)"      style="background-color: skyblue;    color: black;    padding: 5px;">
+      <template slot-scope="{ row }">
+        <el-button
+          type="button"
+          @click="findAllChatHistory(row.memberId)"
+          style="background-color: skyblue; color: black; padding: 5px"
+        >
           전체 조회
         </el-button>
 
-
-
-
-        <el-button    type="button"    @click="findSuccessedChatHistory(row.memberId)"     style="background-color: skyblue;    color: black;    padding: 5px;">
+        <el-button
+          type="button"
+          @click="findSuccessedChatHistory(row.memberId)"
+          style="background-color: skyblue; color: black; padding: 5px"
+        >
           성공한 기록만 조회
         </el-button>
-      
-
-        <!-- <el-tag :type="row.status | statusFilter">
-          {{ row.status }}
-        </el-tag> -->
       </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script>
-import axios from 'axios'
-import { transactionList } from '@/api/remote-search'
+import axios from "axios";
+import { transactionList } from "@/api/remote-search";
 
 export default {
   filters: {
     statusFilter(status) {
       const statusMap = {
-        success: 'success',
-        pending: 'danger'
-      }
-      return statusMap[status]
+        success: "success",
+        pending: "danger",
+      };
+      return statusMap[status];
     },
     orderNoFilter(str) {
-      return str.substring(0, 30)
-    }
+      return str.substring(0, 30);
+    },
   },
   data() {
     return {
-      list: null
-    }
+      list: null,
+    };
   },
   created() {
     // console.log(window["aimedisonkeyworduserlist"]);
 
-    this.fetchData()
+    this.fetchData();
   },
   methods: {
-
     async findSuccessedChatHistory(userId) {
-      console.log(userId)
-      console.log(this.$store.state.aimedison.aimedisonKeywordUserList)
-      let selectedUser = this.$store.state.aimedison.aimedisonKeywordUserList.filter((item) => item.memberId === userId)[0]
-
-      // selectedUser.successHistoryList[selectedUser.successHistoryList.length - 1].trialIdForEvent
-      // "/find/successedchathistory"
+      console.log(userId);
+      console.log(this.$store.state.aimedison.aimedisonKeywordUserList);
+      let selectedUser =
+        this.$store.state.aimedison.aimedisonKeywordUserList.filter(
+          (item) => item.memberId === userId
+        )[0];
 
       this.$store.commit("aimedison/setCurrentSelectedUserId", userId);
-      console.log(selectedUser)
+      console.log(selectedUser);
 
-      if(selectedUser.successHistoryList.length > 0) {
-        let recentSuccessedChatHistory = await axios.post("http://175.119.224.227:5003/find/successedchathistory", {
-          shopId: window.cafe24aimedisonkeywordappshopid,
-          userId: userId,
-          trialId: selectedUser.successHistoryList[selectedUser.successHistoryList.length - 1].trialIdForEvent
-        })
+      if (selectedUser.successHistoryList.length > 0) {
+        let recentSuccessedChatHistory = await axios.post(
+          "http://175.119.224.227:5003/find/successedchathistory",
+          {
+            shopId: window.cafe24aimedisonkeywordappshopid,
+            userId: userId,
+            trialId:
+              selectedUser.successHistoryList[
+                selectedUser.successHistoryList.length - 1
+              ].trialIdForEvent,
+          }
+        );
 
-
-
-        console.log(recentSuccessedChatHistory)
-        console.log(recentSuccessedChatHistory.data)
-        this.$store.commit("aimedison/setRecentScsdChatHistoryOfSlctdUser", recentSuccessedChatHistory.data);
+        console.log(recentSuccessedChatHistory);
+        console.log(recentSuccessedChatHistory.data);
+        this.$store.commit(
+          "aimedison/setRecentScsdChatHistoryOfSlctdUser",
+          recentSuccessedChatHistory.data
+        );
       } else {
         this.$store.commit("aimedison/setRecentScsdChatHistoryOfSlctdUser", []);
       }
-
-
-
-
     },
 
-    
     async findAllChatHistory(userId) {
-      console.log(userId)
+      console.log(userId);
       this.$store.commit("aimedison/setCurrentSelectedUserId", userId);
 
+      let recentSuccessedChatHistory = await axios.post(
+        "http://175.119.224.227:5003/find/allchathistory",
+        {
+          shopId: window.cafe24aimedisonkeywordappshopid,
+          userId: userId,
+        }
+      );
 
-      let recentSuccessedChatHistory = await axios.post("http://175.119.224.227:5003/find/allchathistory", {
-        shopId: window.cafe24aimedisonkeywordappshopid,
-        userId: userId,
-      
-      })
-
-
-      
-
-      console.log(recentSuccessedChatHistory)
-      console.log(recentSuccessedChatHistory.data)
-      this.$store.commit("aimedison/setRecentScsdChatHistoryOfSlctdUser", recentSuccessedChatHistory.data);
-
+      console.log(recentSuccessedChatHistory);
+      console.log(recentSuccessedChatHistory.data);
+      this.$store.commit(
+        "aimedison/setRecentScsdChatHistoryOfSlctdUser",
+        recentSuccessedChatHistory.data
+      );
     },
 
-
-
-
-
-    fetchData() {
-
-
-
-
-
-
-      // transactionList().then(response => {
-      //   this.list = response.data.items.slice(0, 8)
-      //   console.log(response.data.items)
-      //   console.log(this.list)
-      // })
-    }
-  }
-}
+    fetchData() {},
+  },
+};
 </script>
