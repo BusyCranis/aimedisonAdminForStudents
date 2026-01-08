6418a15ceb9c68e094059fd0008e3b3a3a3c7bda
cranis <cranis2@naver.com>
2026-01-08 15:50:33 +0900

    awefawefefwwaefaewfaefwafweafweawefawefwaefefaw

--- src/views/dashboard/admin/components/BarChart.vue
+++ src/views/dashboard/admin/components/BarChart.vue
@@ -1,127 +1,115 @@
 <template>
-  <div :class="className" :style="{height:height,width:width}" />
+  <div :class="className" :style="{ height: height, width: width }" />
 </template>
 
 <script>
-import echarts from 'echarts'
-require('echarts/theme/macarons') // echarts theme
-import resize from './mixins/resize'
+import echarts from "echarts";
+require("echarts/theme/macarons"); // echarts theme
+import resize from "./mixins/resize";
 // import axios from 'axios'
 
-const animationDuration = 6000
+const animationDuration = 6000;
 
 export default {
   mixins: [resize],
   props: {
     className: {
       type: String,
-      default: 'chart'
+      default: "chart",
     },
     width: {
       type: String,
-      default: '100%'
+      default: "100%",
     },
     height: {
       type: String,
-      default: '300px'
-    }
+      default: "300px",
+    },
   },
   data() {
     return {
       chart: null,
 
-      dailyStatResponse: {}
-
-    }
+      dailyStatResponse: {},
+    };
   },
-  created() {
-
-  },
+  created() {},
 
   async mounted() {
-    console.log(this.$el)
+    console.log(this.$el);
   },
   beforeDestroy() {
     if (!this.chart) {
-      return
+      return;
     }
-    this.chart.dispose()
-    this.chart = null
+    this.chart.dispose();
+    this.chart = null;
   },
   methods: {
     initChart() {
-      console.log(this.dailyStatResponse.data)
-      const monthlyKeyNamesForAggr = Object.keys(this.dailyStatResponse.data)
-      const dailyValueArray = []
+      console.log(this.dailyStatResponse.data);
+      const monthlyKeyNamesForAggr = Object.keys(this.dailyStatResponse.data);
+      const dailyValueArray = [];
 
-      console.log(monthlyKeyNamesForAggr)
+      console.log(monthlyKeyNamesForAggr);
 
       for (let i = 0; i < monthlyKeyNamesForAggr.length; i++) {
-        console.log(this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]])
+        console.log(this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]]);
 
-        dailyValueArray.push(this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]].length)
+        dailyValueArray.push(
+          this.dailyStatResponse.data[monthlyKeyNamesForAggr[i]].length
+        );
       }
 
-      this.chart = echarts.init(this.$el, 'macarons')
+      this.chart = echarts.init(this.$el, "macarons");
 
-      console.log(dailyValueArray)
+      console.log(dailyValueArray);
 
       this.chart.setOption({
         tooltip: {
-          trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-          }
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
         },
         grid: {
           top: 10,
-          left: '2%',
-          right: '2%',
-          bottom: '3%',
-          containLabel: true
+          left: "2%",
+          right: "2%",
+          bottom: "3%",
+          containLabel: true,
         },
-        xAxis: [{
-          type: 'category',
-          // data: ['월요일', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-          data: monthlyKeyNamesForAggr,
-          axisTick: {
-            alignWithLabel: true
-          }
-        }],
-        yAxis: [{
-          type: 'value',
-          axisTick: {
-            show: false
-          }
-        }],
-        series: [{
-          name: '대화 수',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          // data: [79, 52, 200, 134, 190, 130],
-          data: dailyValueArray,
-          animationDuration
-        }
-        // {
-        //   name: 'pageB',
-        //   type: 'bar',
-        //   stack: 'vistors',
-        //   barWidth: '60%',
-        //   data: [80, 52, 200, 334, 390, 330, 220],
-        //   animationDuration
-        // }, {
-        //   name: 'pageC',
-        //   type: 'bar',
-        //   stack: 'vistors',
-        //   barWidth: '60%',
-        //   data: [30, 52, 200, 334, 390, 330, 220],
-        //   animationDuration
-        // }
-        ]
-      })
-    }
-  }
-}
+        xAxis: [
+          {
+            type: "category",
+            // data: ['월요일', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            data: monthlyKeyNamesForAggr,
+            axisTick: {
+              alignWithLabel: true,
+            },
+          },
+        ],
+        yAxis: [
+          {
+            type: "value",
+            axisTick: {
+              show: false,
+            },
+          },
+        ],
+        series: [
+          {
+            name: "대화 수",
+            type: "bar",
+            stack: "vistors",
+            barWidth: "60%",
+            // data: [79, 52, 200, 134, 190, 130],
+            data: dailyValueArray,
+            animationDuration,
+          },
+        ],
+      });
+    },
+  },
+};
 </script>
-
